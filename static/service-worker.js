9e35c87c4d2b97372548c02caf0ff421b2d79adc
Prollad Mondal <prolladmail@gmail.com>
2024-07-26 22:39:46 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -26,21 +26,23 @@ self.addEventListener('activate', function(event) {
           return caches.delete(cacheName);
         })
       );
-    })
+    }).then(() => self.clients.claim())
   );
-
-  return self.clients.claim(); // Take control of uncontrolled clients immediately
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(fetchResponse) {
-        return caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        });
+    fetch(event.request).then(function(response) {
+      if (!response || response.status !== 200 || response.type !== 'basic') {
+        return response;
+      }
+      const responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, responseToCache);
       });
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
     })
   );
 });
